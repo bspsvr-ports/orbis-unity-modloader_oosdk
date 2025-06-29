981530d90b817d5e6c71e4dc3a7e85b73076f080
Kaitlyn <ItsKaitlyn03@protonmail.com>
2025-06-28 23:04:13 -0400

    Import from ps4-hen-plugins

---
+++ source/lib.cpp
@@ -0,0 +1,12 @@
+#include "../../common/plugin_common.h"
+#include <stdint.h>
+
+attr_public const char* g_pluginName = "plugin_loader";
+attr_public const char* g_pluginDesc = "Plugin loader.";
+attr_public const char* g_pluginAuth = "illusiony";
+attr_public uint32_t g_pluginVersion = 0x00000100;  // 1.00
+
+extern "C" int plugin_load(int* argc, const char** argv)
+{
+    return 0;
+}
